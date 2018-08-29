ed53a07a0382a3ede01ecb969ac28a5bbd40c538
Tim Schoof <tim.schoof@desy.de>
2018-08-29 16:20:44 +0200

    Implement peak fitting processor

--- BeamlineStatusLogger/processors.py
+++ BeamlineStatusLogger/processors.py
@@ -1,2 +1,53 @@
-def adder(x):
-    return x + 1
+import BeamlineStatusLogger.utils as utils
+
+
+class PeakFitter:
+    """
+        A processor that expects an image and returns the parameters of a
+        Gausian fit
+
+        The instance must be called with a `data` object which `value` field
+        contains a 2d array. The call returns a `data` object which `value`
+        field contains a dict with the following keys:
+
+        beam_on : Boolean
+            Indicates the success of the fitting procedure
+        mu_x, mu_y : float
+            The xy coordinates of the peak maximum
+        sigma_x, sigma_y : float
+            The standard deviation in x and y direction
+        rotation : float
+            The rotation angle (counter-clockwise)
+        z_offset : float
+            An estimate for the background value
+        amplitude : float
+            An estimate for the amplitude of the peak
+        cutoff : float
+            The maximum value of the peak
+    """
+    def __call__(self, data):
+        img = data.value
+
+        try:
+            p_fit = utils.get_peak_parameters(img)
+        except utils.FittingError:
+            p_fit = None
+
+        if p_fit:
+            h, a, x0, y0, sx, sy, theta, cutoff = p_fit
+            d = {"beam_on": True,
+                 "mu_x": x0,
+                 "mu_y": y0,
+                 "sigma_x": sx,
+                 "sigma_y": sy,
+                 "rotation": theta,
+                 "z_offset": h,
+                 "amplitude": a,
+                 "cutoff": cutoff
+                 }
+        else:
+            d = {"beam_on": False}
+
+        data.value = d
+
+        return data
--- tests/test_processors.py
+++ tests/test_processors.py
@@ -1,5 +1,37 @@
-from BeamlineStatusLogger.processors import adder
+from BeamlineStatusLogger.processors import PeakFitter
+from BeamlineStatusLogger.sources import Data
+import BeamlineStatusLogger.utils as utils
+import numpy as np
+from datetime import datetime
 
 
-def test_adder():
-    assert adder(1) == 2
+class TestPeakFitter:
+    def test_peak_fitter_no_beam(self):
+        data = Data(datetime(2018, 8, 28), np.random.randn(600, 800),
+                    metadata={"id": 1234})
+        pf = PeakFitter()
+        proc_data = pf(data)
+        assert proc_data.timestamp == data.timestamp
+        assert proc_data.value["beam_on"] is False
+        assert proc_data.metadata["id"] == 1234
+
+    def test_get_peak_fitter_beam(self, monkeypatch):
+        def mockreturn(img):
+                return 0, 1, 2, 3, 4, 5, 6, 7
+        monkeypatch.setattr(utils, 'get_peak_parameters', mockreturn)
+
+        data = Data(datetime(2018, 8, 28), np.random.randn(600, 800),
+                    metadata={"id": 1234})
+        pf = PeakFitter()
+        proc_data = pf(data)
+        assert proc_data.timestamp == data.timestamp
+        assert proc_data.value["beam_on"] is True
+        assert proc_data.value["mu_x"] == 2
+        assert proc_data.value["mu_y"] == 3
+        assert proc_data.value["sigma_x"] == 4
+        assert proc_data.value["sigma_y"] == 5
+        assert proc_data.value["rotation"] == 6
+        assert proc_data.value["z_offset"] == 0
+        assert proc_data.value["amplitude"] == 1
+        assert proc_data.value["cutoff"] == 7
+        assert proc_data.metadata["id"] == 1234
