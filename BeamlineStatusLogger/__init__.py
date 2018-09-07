e426c1e9c00a413ed0c38d74cd9c77875376cec1
Tim Schoof <tim.schoof@desy.de>
2018-09-07 19:15:46 +0200

    Export submodules from top module

---
+++ BeamlineStatusLogger/__init__.py
@@ -0,0 +1,10 @@
+from BeamlineStatusLogger.logger import Logger
+from BeamlineStatusLogger import sources, processors, sinks, timer
+
+__all__ = [
+    "sources",
+    "processors",
+    "sinks",
+    "timer",
+    "Logger",
+]
