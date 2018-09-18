ce55b921ec6ee7b075b8d4f126d917596f5fea2d
Tim Schoof <tim.schoof@desy.de>
2018-09-18 13:23:53 +0200

    Use setup.py to install systemd units and config files

--- setup.py
+++ setup.py
@@ -1,4 +1,45 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools.command.install import install
+from distutils import log
+import os
+import glob
+
+CONFDIR = "/etc/beamline_status_logger"
+SCRIPTNAME = "beamline_status_logger"
+SERVICEPATH = "/lib/systemd/system/beamline_status_logger@.service"
+
+
+def install_system_service(infile, outfile, script, confdir):
+    with open(infile) as fin:
+        template = fin.read()
+
+    with open(outfile, "w") as fout:
+        fout.write(template.format(script=script, confdir=confdir))
+
+
+class CustomInstall(install):
+    def run(self):
+        super().run()
+        if SERVICEPATH.startswith("/"):
+            servicepath = os.path.join(self.root, SERVICEPATH[1:])
+        else:
+            servicepath = os.path.join(self.root, SERVICEPATH)
+        log.info("writing " + servicepath)
+        if not self.dry_run:
+            install_system_service(
+                "config/beamline_status_logger@.service.in",
+                servicepath,
+                self.get_scriptfile_path(),
+                os.path.join(CONFDIR, "")
+            )
+
+    def get_scriptfile_path(self):
+        bindir = self.install_scripts
+        rootdir = self.root
+        if rootdir and bindir.startswith(rootdir):
+            bindir = bindir[len(rootdir):]
+        return os.path.join(bindir, SCRIPTNAME)
+
 
 setup(name="BeamlineStatusLogger",
       version="0.0.1",
@@ -8,10 +49,19 @@ setup(name="BeamlineStatusLogger",
       packages=["BeamlineStatusLogger", "tests"],
       install_requires=[
           "PyTango",  # PyTango takes very long to install by pip
-          "influxdb",
+          "influxdb",  # not in Debian stretch
           "scipy",
           "scikit-image",
           "pytz"
       ],
-      scripts=["bin/beam_parameter_logger", "bin/beamline_status_logger"],
+      zip_safe=False,
+      data_files=[
+            ("/lib/systemd/system", ["config/beamline_status_logger.target"]),
+            (CONFDIR, (glob.glob("config/*.logger")
+                       + glob.glob("config/*.base")))
+      ],
+      scripts=[os.path.join("bin/", SCRIPTNAME)],
+      cmdclass={
+            'install': CustomInstall
+      },
       )
