c82c903db839f8722388dc81f44278084107973f
Tim Schoof <tim.schoof@desy.de>
2019-10-18 17:25:26 +0200

    Add limited support for TINE as a data source

--- BeamlineStatusLogger/sources.py
+++ BeamlineStatusLogger/sources.py
@@ -1,5 +1,12 @@
 import PyTango as tango
+try:
+    import PyTine as tine
+except ImportError as err:
+    tine = None
+    tine_import_err = err
 from concurrent import futures
+from datetime import datetime
+import numpy as np
 from pytz import timezone
 
 
@@ -77,4 +84,115 @@ class TangoDeviceAttributeSource:
         value = {self.attribute_name: device_attribute.value}
         metadata = self.metadata.copy()
         metadata["quality"] = str(device_attribute.quality)
+        return Data(timestamp, value, metadata=metadata)
+
+
+def get_tine_image_height(frameHeader):
+    if frameHeader["aoiHeight"] > 0:
+        height = frameHeader["aoiHeight"]
+    else:
+        height = frameHeader["sourceHeight"]
+
+    if not 0 < height <= 65535:
+        raise ValueError("Invalid height = {}".format(height))
+
+    return height
+
+
+def get_tine_image_width(frameHeader):
+    if frameHeader["aoiWidth"] > 0:
+        width = frameHeader["aoiWidth"]
+    else:
+        width = frameHeader["sourceWidth"]
+
+    if not 0 < width <= 65535:
+        raise ValueError("Invalid width = {}".format(width))
+
+    return width
+
+
+def get_tine_image_dtype(frameHeader):
+    # TODO: Endianness?
+    if frameHeader["bytesPerPixel"] == 1:
+        dtype = "u1"
+    elif frameHeader["bytesPerPixel"] == 2:
+        dtype = "u2"
+    else:
+        raise ValueError("Invalid bytesPerPixel = {}".format(
+            frameHeader["bytesPerPixel"]))
+
+    return dtype
+
+
+def tine_image_to_numpy(data):
+    frameHeader = data["frameHeader"]
+
+    height = get_tine_image_height(frameHeader)
+    width = get_tine_image_width(frameHeader)
+    dtype = get_tine_image_dtype(frameHeader)
+
+    bytes = np.frombuffer(data["imageBytes"], dtype=dtype)
+
+    if len(bytes) != height*width:
+        raise ValueError(
+            "Dimension mismatch: len(bytes) = {}, height*width = {}".format(
+                len(bytes), height*width))
+
+    return bytes.reshape((height, width))
+
+
+class TINECameraSource:
+    """A specialized interface to the TINE beam position cameras
+
+    Parameters
+    ----------
+    device_address : string
+        Name of the device
+    property_name : string
+        Name of the property
+    metadata : dict_like
+        The metadata is added to every returned data object
+    """
+    def __init__(self, device_address, property_name, metadata={}, tz=None):
+        if tine is None:
+            raise tine_import_err
+        self.device_address = device_address
+        self.property_name = property_name
+        # Test if device and property exists
+        tine.get(self.device_address, self.property_name)
+        self.metadata = metadata
+        if "status" in self.metadata:
+            raise ValueError("The metadata entry 'status' is reserved for the"
+                             "readback value key of the same name. Choose"
+                             "a different name instead.")
+        if tz:
+            self.localtz = tz
+        else:
+            self.localtz = timezone('Europe/Berlin')
+
+    def read(self):
+        try:
+            device_property = tine.get(self.device_address, self.property_name)
+        except (OSError, RuntimeError) as err:
+            # TODO: Check if this is close enough to the would be time of
+            #       a successful read
+            timestamp = datetime.now()
+            timestamp = self.localtz.localize(timestamp)
+            return Data(timestamp, None, err, metadata=self.metadata.copy())
+
+        status = tine.strerror(device_property["status"])
+        metadata = self.metadata.copy()
+        metadata["status"] = status
+        if status.endswith(": success"):
+            timestamp = datetime.fromtimestamp(device_property["timestamp"])
+            timestamp = self.localtz.localize(timestamp)
+            try:
+                img = tine_image_to_numpy(device_property["data"])
+            except ValueError as err:
+                return Data(timestamp, None, err, metadata=metadata)
+            value = {self.property_name: img}
+        else:
+            timestamp = datetime.now()
+            timestamp = self.localtz.localize(timestamp)
+            value = None
         return Data(timestamp, value, metadata=metadata)
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,4 +1,7 @@
-from BeamlineStatusLogger.sources import TangoDeviceAttributeSource
+from BeamlineStatusLogger import sources
+from BeamlineStatusLogger.sources import (
+    TangoDeviceAttributeSource, TINECameraSource)
+import numpy as np
 import PyTango as tango
 import datetime
 from pytz import timezone
@@ -72,3 +75,198 @@ class TestTangoDeviceAttributeSource:
         assert data.value is None
         assert data.metadata is not s.metadata
         assert data.metadata["attribute"] == attribute_name
+
+
+@pytest.fixture
+def tine_data():
+    img = np.arange(15, dtype="u1").reshape(3, 5)
+    frameHeader = dict(
+        aoiHeight=-1,
+        sourceHeight=3,
+        aoiWidth=-1,
+        sourceWidth=5,
+        bytesPerPixel=1
+    )
+    bytes = img.tobytes()
+    data = dict(
+        frameHeader=frameHeader,
+        imageBytes=bytes
+    )
+    reply = dict(
+        status=0,
+        data=data,
+        timestamp=1571409806.054523
+    )
+    return img, reply
+
+
+class TestTINECameraImage:
+    def test_get_height(self):
+        height = 1234
+        frameHeader = dict(
+            aoiHeight=-1,
+            sourceHeight=height)
+        assert sources.get_tine_image_height(frameHeader) == height
+
+    def test_get_height_aoi(self):
+        height = 1234
+        frameHeader = dict(
+            aoiHeight=height,
+            sourceHeight=2*height)
+        assert sources.get_tine_image_height(frameHeader) == height
+
+    def test_get_height_invalid(self):
+        height = -1
+        frameHeader = dict(
+            aoiHeight=height,
+            sourceHeight=height)
+        with pytest.raises(ValueError):
+            sources.get_tine_image_height(frameHeader)
+
+    def test_get_width(self):
+        width = 1234
+        frameHeader = dict(
+            aoiWidth=-1,
+            sourceWidth=width)
+        assert sources.get_tine_image_width(frameHeader) == width
+
+    def test_get_width_aoi(self):
+        width = 1234
+        frameHeader = dict(
+            aoiWidth=width,
+            sourceWidth=2*width)
+        assert sources.get_tine_image_width(frameHeader) == width
+
+    def test_get_width_invalid(self):
+        width = -1
+        frameHeader = dict(
+            aoiWidth=width,
+            sourceWidth=width)
+        with pytest.raises(ValueError):
+            sources.get_tine_image_width(frameHeader)
+
+    @pytest.mark.parametrize(
+        ("bytesPerPixel", "dtype"), [(1, "u1"), (2, "u2")])
+    def test_get_dtype(self, bytesPerPixel, dtype):
+        frameHeader = dict(bytesPerPixel=bytesPerPixel)
+        assert sources.get_tine_image_dtype(frameHeader) == dtype
+
+    def test_get_dtype_unknown(self):
+        frameHeader = dict(bytesPerPixel=4)
+        with pytest.raises(ValueError):
+            sources.get_tine_image_dtype(frameHeader)
+
+    def test_get_tine_image(self, tine_data):
+        img, reply = tine_data
+        data = reply["data"]
+        np.testing.assert_array_equal(sources.tine_image_to_numpy(data), img)
+
+    def test_get_tine_image_mismatch(self, tine_data):
+        img, reply = tine_data
+        data = reply["data"]
+        data["frameHeader"]["sourceHeight"] = 4
+        with pytest.raises(ValueError):
+            sources.tine_image_to_numpy(data)
+
+
+class TestTINECameraSource:
+    dummy_address = "/CONTEXT/server/device"
+    dummy_property = "frame"
+
+    @pytest.fixture(autouse=True)
+    def tine_mock(self, mocker, tine_data):
+        img, reply = tine_data
+        mock = mocker.patch.object(sources, "tine")
+
+        def get(device_name, property_name):
+            if (device_name != self.dummy_address
+                    or property_name != self.dummy_property):
+                raise RuntimeError("Unknown Device")
+            return reply
+        mock.get.side_effect = get
+
+        def strerror(status):
+            if status == 0:
+                return "RMT: success"
+            else:
+                return "RMT: failure"
+        mock.strerror.side_effect = strerror
+
+        return mock
+
+    def test_init_success(self):
+        s = TINECameraSource(self.dummy_address, self.dummy_property)
+        assert s.device_address == self.dummy_address
+        assert s.property_name == self.dummy_property
+        assert s.metadata == {}
+
+    def test_init_failure(self):
+        device_address = "/CONTEXT/server/other_device"
+        property_name = "frame"
+        with pytest.raises(RuntimeError):
+            TINECameraSource(device_address, property_name)
+
+    def test_init_metadata_contains_status(self):
+        with pytest.raises(ValueError):
+            TINECameraSource(
+                self.dummy_address, self.dummy_property,
+                metadata={"status": "bad"})
+
+    def test_read_success(self, tine_data):
+        img, reply = tine_data
+        s = TINECameraSource(self.dummy_address, self.dummy_property,
+                             metadata={"device": self.dummy_address})
+        data = s.read()
+        timestamp = timezone("Europe/Berlin").localize(
+            datetime.datetime.fromtimestamp(
+                reply["timestamp"]))
+        assert data.timestamp == timestamp
+        assert data.failure is None
+        np.testing.assert_array_equal(data.value[self.dummy_property], img)
+        assert data.metadata is not s.metadata
+        assert data.metadata["device"] == self.dummy_address
+
+    def test_read_error(self, tine_mock):
+        maxdelta = datetime.timedelta(seconds=5)
+        s = TINECameraSource(self.dummy_address, self.dummy_property,
+                             metadata={"device": self.dummy_address})
+        ex = RuntimeError("Some error")
+        tine_mock.get.side_effect = ex
+        data = s.read()
+        now = datetime.datetime.now()
+        now = timezone("Europe/Berlin").localize(now)
+        assert data.timestamp - now < maxdelta
+        assert data.failure is ex
+        assert data.value is None
+        assert data.metadata is not s.metadata
+        assert data.metadata["device"] == self.dummy_address
+
+    def test_read_wrong_status(self, tine_mock):
+        maxdelta = datetime.timedelta(seconds=5)
+        s = TINECameraSource(self.dummy_address, self.dummy_property,
+                             metadata={"device": self.dummy_address})
+        tine_mock.get.side_effect = None
+        tine_mock.get.return_value = dict(status=1)
+        data = s.read()
+        now = datetime.datetime.now()
+        now = timezone("Europe/Berlin").localize(now)
+        assert data.timestamp - now < maxdelta
+        assert data.failure is None
+        assert data.value is None
+        assert data.metadata is not s.metadata
+        assert data.metadata["device"] == self.dummy_address
+
+    def test_read_corrupted_data(self, tine_data):
+        img, reply = tine_data
+        reply["data"]["imageBytes"] = b"bad"
+        s = TINECameraSource(self.dummy_address, self.dummy_property,
+                             metadata={"device": self.dummy_address})
+        data = s.read()
+        timestamp = timezone("Europe/Berlin").localize(
+            datetime.datetime.fromtimestamp(
+                reply["timestamp"]))
+        assert data.timestamp == timestamp
+        assert isinstance(data.failure, ValueError)
+        assert data.value is None
+        assert data.metadata is not s.metadata
+        assert data.metadata["device"] == self.dummy_address
