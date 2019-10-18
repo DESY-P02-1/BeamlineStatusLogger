b446954232e80ff533c56e74d4e480e200b67d0a
Tim Schoof <tim.schoof@desy.de>
2019-10-18 17:26:22 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class CustomInstall(install):
 
 
 setup(name="BeamlineStatusLogger",
-      version="0.1.0",
+      version="0.2.0",
       description="Log data from a source to a sink",
       author="Tim Schoof",
       author_email="tim.schoof@desy.de",
